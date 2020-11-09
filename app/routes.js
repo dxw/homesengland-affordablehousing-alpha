64a4504da72d842459031e32653af73a843cff92
Robbie Paul <robbie.paul@dxw.com>
2020-11-09 16:51:37 +0000

    Add branching for partnership type and why question

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.post('/apply/partnership', function (req, res) {
+  if (req.body['is-partnership'] === 'yes') {
+    return res.redirect('/apply/partnership-type')
+  } else {
+    return res.redirect('/apply/why')
+  }
+})
 
 module.exports = router
