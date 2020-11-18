f34d49a7ef54094055ca7e56cb8fe1fb9c41c2ce
Robbie Paul <robbie.paul@dxw.com>
2020-11-18 17:43:31 +0000

    Seperate costs and funding to their own tasks

--- app/routes.js
+++ app/routes.js
@@ -18,32 +18,4 @@ router.post('/apply/partnership', function (req, res) {
   }
 })
 
-router.all('/apply/regions', function (req, res) {
-  const regions = req.session.data['homes-locations']
-  const tabs = ['total-cost', 'funding']
-
-  const currentTab = req.body.currentTab
-  let region = req.body.region
-  let nextTab = tabs[tabs.indexOf(currentTab) + 1]
-
-  if (currentTab === tabs[tabs.length - 1]) {
-    nextTab = tabs[0]
-    region = regions[regions.indexOf(region) + 1]
-  }
-
-  // No current tab set so redirect to the first page of the first region
-  if (!currentTab) {
-    return res.redirect('/apply/' + tabs[0] + '?region=' + regions[0])
-  }
-
-  // No more regions left to input data for so redirect to the next page in the journey
-  if (region === undefined) {
-    req.session.data.tasks.costsFundingTimeline = 'completed'
-    return res.redirect('/apply/tasks')
-  }
-
-  req.session.data.tasks.costsFundingTimeline = 'in_progress'
-  return res.redirect('/apply/' + nextTab + '?region=' + region)
-})
-
 module.exports = router
